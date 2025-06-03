6f95dfaf5fe4ba603ec65fd74f91ece668a4111c
ALAN PRAFUL <alanpraful2006@gmail.com>
2025-06-04 02:04:10 +0530

    Update socket URL and build for Netlify

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,42 @@
 
-import './App.css';
-import io from 'socket.io-client'
-import React, { useState } from 'react';
-import Chat from './Chat'
+  import './App.css';
+  import io from 'socket.io-client'
+  import React, { useState } from 'react';
+  import Chat from './Chat'
 
 
-const socket = io.connect("http://localhost:3001")
-function App() {
+  const socket = io.connect("https://chatting-app-1-8cyo.onrender.com")
+  function App() {
 
-  const [ userName , setUserName] =  useState("")
-  const [ room , setRoom ] =  useState("")
-  const [ showChat , setShowChat ] = useState(false)
+    const [ userName , setUserName] =  useState("")
+    const [ room , setRoom ] =  useState("")
+    const [ showChat , setShowChat ] = useState(false)
 
-  const joinRoom = () => {
-    if(userName !== "" && room !== ""){
-      socket.emit("join_room" , room)
-      setShowChat(true);
-    }  
+    const joinRoom = () => {
+      if(userName !== "" && room !== ""){
+        socket.emit("join_room" , room)
+        setShowChat(true);
+      }  
+    }
+    return (
+      <div className="App">
+        {!showChat ? (  
+        <div className= "joinChatContainer">
+
+        <h3>Join a Chat</h3>
+        <input type='text' placeholder='Enter Your Name' onChange = {(event) => {setUserName(event.target.value)}}/>
+        <input type='text' placeholder='Enter Room Id'  onChange = {(event) => {setRoom(event.target.value)}}/>
+        <button onClick={joinRoom}>Join Room</button>
+        </div>
+        )
+        :
+        (
+        <Chat socket = {socket} userName = {userName} room = {room}/>
+        )}
+
+        </div>
+      
+    );
   }
-  return (
-    <div className="App">
-      {!showChat ? (  
-      <div className= "joinChatContainer">
 
-      <h3>Join a Chat</h3>
-      <input type='text' placeholder='Enter Your Name' onChange = {(event) => {setUserName(event.target.value)}}/>
-      <input type='text' placeholder='Enter Room Id'  onChange = {(event) => {setRoom(event.target.value)}}/>
-      <button onClick={joinRoom}>Join Room</button>
-      </div>
-      )
-      :
-      (
-      <Chat socket = {socket} userName = {userName} room = {room}/>
-      )}
-
-      </div>
-    
-  );
-}
-
-export default App;
+  export default App;
